cc16aa5d65357afd608b836e5ddddfdfab78fe21
el2e10 <eldhoittangeorge@gmail.com>
2024-07-19 01:08:17 +0530

    Start expression parsing

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,18 +31,51 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
+// For parsing expressions like x + 10
+type ExpressionStatement struct {
+	Token      token.Token // Storing first token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
 
 type LetStatement struct {
-	Token token.Token // Tokens associated with the let statement let in the sentence let x = 2 + 5
-	Name  *Identifier //  x in 'let x = 2 + 5'
-	Value Expression  // 2 + 5
+	Token       token.Token // Tokens associated with the let statement let in the sentence let x = 2 + 5
+	Name        *Identifier //  x in 'let x = 2 + 5'
+	ReturnValue Expression  // 2 + 5
 }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+func (ls *LetStatement) String() string {
 
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.ReturnValue != nil {
+		out.WriteString(ls.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+
+}
 
 type ReturnStatement struct {
 	Token token.Token
@@ -51,7 +86,20 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+func (rs *ReturnStatement) String() string {
 
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.Value != nil {
+		out.WriteString(rs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 func (p *Program) TokenLiteral() string {
 
@@ -60,5 +108,14 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 
 }
